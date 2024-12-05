2a27502b430fe8e7b658fab2ee860a9b2eb9055a
IndeecFOX <44396407+IndeecFOX@users.noreply.github.com>
2024-12-05 08:54:20 +0300

    Create fast_install.sh

---
+++ fast_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Обновление пакетов и установка unzip
+apt update && apt install -y unzip
+
+# Переход в директорию /opt
+cd /opt
+
+# Скачивание и распаковка архива
+wget https://github.com/IndeecFOX/zapret4rocket/releases/download/Z4R/zapret.zip
+unzip zapret.zip
+
+# Установка прав на выполнение
+#find zapret/* -type f -exec chmod +x {} \;
+
+# Запуск установочных скриптов
+sh zapret/install_bin.sh
+sh zapret/install_prereq.sh
+sh zapret/install_easy.sh
+
+# Перезапуск сервиса zapret
+zapret/init.d/sysv/zapret restart
