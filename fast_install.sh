405dee5e7c69ddc39e1bbeb9e48679434ac6c4e5
IndeecFOX <44396407+IndeecFOX@users.noreply.github.com>
2024-12-25 00:46:21 +0300

    Update fast_install.sh

--- fast_install.sh
+++ fast_install.sh
@@ -7,16 +7,17 @@ apt update && apt install -y unzip
 # Переход в директорию /opt
 cd /opt
 
-# Скачивание и распаковка архива
-wget https://github.com/IndeecFOX/zapret4rocket/releases/download/Z4R/zapret.zip
-unzip zapret.zip
+# Скачивание и распаковка архива zapret 69.8
+wget https://github.com/bol-van/zapret/releases/download/v69.8/zapret-v69.8.zip
+unzip zapret-v69.8.zip
 mv zapret-v69.8 zapret
 
 #Включение обхода дискорда
 cp /opt/zapret/init.d/custom.d.examples.linux/50-discord /opt/zapret/init.d/sysv/custom.d/
 
-# Установка прав на выполнение
-#find zapret/* -type f -exec chmod +x {} \;
+#Копирование нашего конфига на замену стандартному
+wget -O config.default https://raw.githubusercontent.com/IndeecFOX/zapret4rocket/master/config.default
+mv config.default /opt/zapret/
 
 # Запуск установочных скриптов
 sh zapret/install_bin.sh
