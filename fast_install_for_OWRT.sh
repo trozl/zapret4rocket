6b943a7805d5e95cf3901f2757a780ead245ac8a
IndeecFOX <44396407+IndeecFOX@users.noreply.github.com>
2025-02-07 21:00:11 +0300

    Update fast_install_for_OWRT.sh

--- fast_install_for_OWRT.sh
+++ fast_install_for_OWRT.sh
@@ -1,12 +1,31 @@
 #SSH Instal command: opkg install bash curl && bash <(curl -Ls https://raw.githubusercontent.com/IndeecFOX/zapret4rocket/refs/heads/master/fast_install_for_OWRT.sh)
 
+#pre
 opkg update
 opkg install unzip
 
+#directories
 cd /
 mkdir opt
 cd /opt
 
+#Удаление старого запрета, если есть
+if [ -f "zapret/uninstall_easy.sh" ]; then
+    echo "Файл zapret/uninstall_easy.sh найден. Выполняем его"
+    sh zapret/uninstall_easy.sh
+    echo "Скрипт uninstall_easy.sh выполнен."
+else
+    echo "Файл zapret/uninstall_easy.sh не найден. Переходим к следующему шагу."
+fi
+if [ -d "zapret" ]; then
+    echo "Удаляем папку zapret"
+    rm -rf zapret
+    echo "Папка zapret успешно удалена."
+else
+    echo "Папка zapret не существует."
+fi
+
+# Распаковка архива zapret и его удаление
 wget -O zapret-v69.9.zip "https://github.com/bol-van/zapret/releases/download/v69.9/zapret-v69.9.zip"
 unzip zapret-v69.9.zip 
 rm -f zapret-v69.9.zip
