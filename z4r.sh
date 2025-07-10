59bbb9ca4683f927c0c13ac03327c0c2339b1260
IndeecFOX <44396407+IndeecFOX@users.noreply.github.com>
2025-07-10 21:44:38 +0300

    В разработке. Русификация и красота по мелочи

--- z4r.sh
+++ z4r.sh
@@ -21,26 +21,26 @@ elif [[ -f /etc/entware_release ]]; then
     source /etc/entware_release
     release=$ID
 else
-    echo "Не удалось определить ОС. Считать, что это Entware Keenetic" >&2
-	release="Entware_Keenetic"
+    echo "Не удалось определить ОС. Прекращение работы скрипта." >&2
+    exit 1
 fi
 echo "OS: $release"
 
 VPS() {
  #Запрос на установку 3x-ui или аналогов
- read -p $'\033[33mInstall tunneling software?\033[0m \033[32m(3xui, marzban, wg, 3proxy or Enter for none): \033[0m' answer
+ read -p $'\033[33mУстановить ПО для туннелирования?\033[0m \033[32m(3xui, marzban, wg, 3proxy or Enter для пропуска): \033[0m' answer
  # Удаляем лишние символы и пробелы, приводим к верхнему регистру
  clean_answer=$(echo "$answer" | tr -d '[:space:]' | tr '[:lower:]' '[:upper:]')
  if [[ -z "$clean_answer" ]]; then
-     echo "Skipping tunneling soft installation (default action)."
+     echo "Пропуск установки ПО туннелирования."
  elif [[ "$clean_answer" == "3XUI" ]]; then
-     echo "Installing 3x-ui panel..."
+     echo "Установка 3x-ui панели."
      bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
  elif [[ "$clean_answer" == "WG" ]]; then
-     echo "Installing wg..."
+     echo "Установка WG (by angristan)"
      bash <(curl -Ls https://raw.githubusercontent.com/angristan/wireguard-install/refs/heads/master/wireguard-install.sh)
  elif [[ "$clean_answer" == "3PROXY" ]]; then
-     echo "Installing 3proxy..."
+     echo "Установка 3proxy (by SnoyIatk)"
      bash <(curl -Ls https://raw.githubusercontent.com/SnoyIatk/3proxy/master/3proxyinstall.sh)
      wget -O /etc/3proxy/.proxyauth https://raw.githubusercontent.com/IndeecFOX/zapret4rocket/refs/heads/master/del.proxyauth
      wget -O /etc/3proxy/3proxy.cfg https://raw.githubusercontent.com/IndeecFOX/zapret4rocket/refs/heads/master/3proxy.cfg
@@ -49,21 +49,22 @@ VPS() {
      #mv 3proxy.cfg /etc/3proxy/
      systemctl restart 3proxy
  elif [[ "$clean_answer" == "MARZBAN" ]]; then
+     echo "Установка Marzban"
      bash -c "$(curl -sL https://github.com/Gozargah/Marzban-scripts/raw/master/marzban.sh)" @ install
  else
-     echo "Skipping tunneling soft installation."
+     echo "Пропуск установки ПО туннелирования."
  fi
 
  #Запрос на подбор стратегий
  if [ -f "/opt/zapret/uninstall_easy.sh" ]; then
-  read -p $'\033[33mNeed find strategy? Write digital or press Enter for skip:\033[0m\n\033[32m1. YT (UDP QUIC)\n2. YT (TCP)\n3. RKN\n4. New domain\nEnter for none\033[0m ' answer
+  read -p $'\033[33mПодобрать альтернативную стратегию? Укажите цифру или нажмите Enter для пропуска:\033[0m\n\033[32m1. YT (UDP QUIC)\n2. YT (TCP)\n3. RKN\n4. Добавить домен\nEnter for none\033[0m ' answer
   clean_answer=$(echo "$answer" | tr -d '[:space:]' | tr '[:lower:]' '[:upper:]') # Удаляем лишние символы и пробелы, приводим к верхнему регистру
 
   if [[ -z "$clean_answer" ]]; then
-      echo "Skipping finding extra strats (default action)"
+      echo "Пропуск подбора альтернативной стратегии"
 
   elif [[ "$clean_answer" == "1" ]]; then
-     echo "Mode: Finding strategy for YT (UDP QUIC) activated"
+     echo "Режим подбора стратегии для YT (UDP QUIC) активирован"
      # Цикл от 1 до 8
      for i in {1..8}; do
 
@@ -76,24 +77,24 @@ VPS() {
 
          systemctl restart zapret
 
-         echo "Strategy $i activated"
+         echo "Стратегия номер $i активирована"
 
-         read -p "Test strategy in browser and write answer here (\"Y\" - Apply strategy, Enter for try next strategy): " answer
+         read -p "Проверьте работоспособность в браузере и т.п. и дайте ответ: (\"Y\" - Сохранить стратегию, Enter - попробовать следующую): " answer
          clean_answer=$(echo "$answer" | tr -d '[:space:]' | tr '[:lower:]' '[:upper:]')
 
          if [[ "$clean_answer" == "Y" ]]; then
-             echo "Strategy $i applied and exiting."
+             echo "Стратегия $i сохранена. Выходим."
              exit 0
          fi
      done
 
      # После цикла
      echo -n > "/opt/zapret/extra_strats/UDP/YT/8.txt"
-     echo "End strategy lists. All was tried"
+     echo "Перечень стратегий закончился. Мы испробовали всё"
      exit 0
 	
   elif [[ "$clean_answer" == "2" ]]; then
-     echo "Mode: Finding strategy for YT (TCP) activated"
+     echo "Режим подбора стратегии для YT (TCP) активирован"
      # Цикл от 1 до 17
      for i in {1..17}; do
 
@@ -106,24 +107,24 @@ VPS() {
 
          systemctl restart zapret
 
-         echo "Strategy $i activated"
+         echo "Стратегия номер $i активирована"
 
-         read -p "Test strategy in browser and write answer here (\"Y\" - Apply strategy, Enter for try next strategy): " answer
+         read -p "Проверьте работоспособность в браузере и т.п. и дайте ответ: (\"Y\" - Сохранить стратегию, Enter - попробовать следующую): " answer
          clean_answer=$(echo "$answer" | tr -d '[:space:]' | tr '[:lower:]' '[:upper:]')
 
          if [[ "$clean_answer" == "Y" ]]; then
-             echo "Strategy $i applied and exiting."
+             echo "Стратегия $i сохранена. Выходим."
              exit 0
          fi
      done
 
      # После цикла
      echo -n > "/opt/zapret/extra_strats/TCP/YT/17.txt"
-     echo "End strategy lists. All was tried"
+     echo "Перечень стратегий закончился. Мы испробовали всё"
      exit 0
     
   elif [[ "$clean_answer" == "3" ]]; then
-     echo "Mode: Finding strategy for RKN lists activated"
+     echo "Режим подбора стратегии для листа РКН активирован"
      # Цикл от 1 до 17
      for i in {1..17}; do
 
@@ -136,26 +137,26 @@ VPS() {
 
          systemctl restart zapret
 
-         echo "Strategy $i activated"
+         echo "Стратегия номер $i активирована"
 
-         read -p "Test strategy in browser and write answer here (\"Y\" - Apply strategy, Enter for try next strategy): " answer
+         read -p "Проверьте работоспособность в браузере и т.п. и дайте ответ: (\"Y\" - Сохранить стратегию, Enter - попробовать следующую): " answer
          clean_answer=$(echo "$answer" | tr -d '[:space:]' | tr '[:lower:]' '[:upper:]')
 
          if [[ "$clean_answer" == "Y" ]]; then
-             echo "Strategy $i applied and exiting."
+             echo "Стратегия $i сохранена. Выходим."
              exit 0
          fi
      done
 
      # После цикла
      echo -n > "/opt/zapret/extra_strats/TCP/RKN/17.txt"
-     echo "End strategy lists. All was tried"
+     echo "Перечень стратегий закончился. Мы испробовали всё"
      exit 0
 
   elif [[ "$clean_answer" == "4" ]]; then
-     echo "Mode: Finding strategy for new domain activated"
+     echo "Режим подбора стратегии для кастомного домена активирован"
 
-	 read -p "Input domain please (example, mydomain.com): " user_domain
+	 read -p "Введите требуемый домен (Например: mydomain.com): " user_domain
 	 user_domain=$(echo "$user_domain" | tr -d '[:space:]')  # удаляем лишние пробелы
      # Цикл от 1 до 17
      for i in {1..17}; do
@@ -178,27 +179,27 @@ VPS() {
 
          systemctl restart zapret
 
-         echo "Strategy $i activated"
+         echo "Стратегия номер $i активирована"
 
-         read -p "Test strategy in browser and write answer here (\"Y\" - Apply strategy, Enter for try next strategy): " answer
+         read -p "Проверьте работоспособность в браузере и т.п. и дайте ответ: (\"Y\" - Сохранить стратегию, Enter - попробовать следующую): " answer
          clean_answer=$(echo "$answer" | tr -d '[:space:]' | tr '[:lower:]' '[:upper:]')
 
          if [[ "$clean_answer" == "Y" ]]; then
 			 echo -n > "/opt/zapret/extra_strats/TCP/temp/${i}.txt" #Очищаем temp
 			 echo "$user_domain" > "/opt/zapret/extra_strats/TCP/User/${i}.txt" #Кидаем в юзерлист на постоянку в приоритет
 			 cp "/opt/zapret/extra_strats/TCP/RKN/List.txt" "/opt/zapret/extra_strats/TCP/RKN/${emp}.txt" #Включить обратно общий лист
-             echo "Strategy $i applied and exiting."
+             echo "Стратегия $i сохранена. Выходим."
              exit 0
          fi
      done
 
      # После цикла
      echo -n > "/opt/zapret/extra_strats/TCP/temp/17.txt"
-     echo "End strategy lists. All was tried"
+     echo "Перечень стратегий закончился. Мы испробовали всё"
      exit 0
  
   else
-     echo "Skipping finding extra strats (default action)"
+     echo "Пропуск подбора альтернативной стратегии"
   fi
  else
   echo "zapret не установлен, пропускаем скрипт подбора профиля"
@@ -255,150 +256,154 @@ VPS() {
 }
 
 WRT() {
- #Запрос на подбор стратегий 
-  read -p $'\033[33mNeed find strategy? Write digital or press Enter for skip:\033[0m\n\033[32m1. YT (UDP QUIC)\n2. YT (TCP)\n3. RKN\n4. New domain\nEnter for none\033[0m ' answer
- clean_answer=$(echo "$answer" | tr -d '[:space:]' | tr '[:lower:]' '[:upper:]') # Удаляем лишние символы и пробелы, приводим к верхнему регистру
- 
- if [[ -z "$clean_answer" ]]; then
-     echo "Skipping finding extra strats (default action)"
- 
- elif [[ "$clean_answer" == "1" ]]; then
-     echo "Mode: Finding strategy for YT (UDP QUIC) activated"
+ #Запрос на подбор стратегий
+ if [ -f "/opt/zapret/uninstall_easy.sh" ]; then
+  read -p $'\033[33mПодобрать альтернативную стратегию? Укажите цифру или нажмите Enter для пропуска:\033[0m\n\033[32m1. YT (UDP QUIC)\n2. YT (TCP)\n3. RKN\n4. Добавить домен\nEnter for none\033[0m ' answer
+  clean_answer=$(echo "$answer" | tr -d '[:space:]' | tr '[:lower:]' '[:upper:]') # Удаляем лишние символы и пробелы, приводим к верхнему регистру
+
+  if [[ -z "$clean_answer" ]]; then
+      echo "Пропуск подбора альтернативной стратегии"
+
+  elif [[ "$clean_answer" == "1" ]]; then
+     echo "Режим подбора стратегии для YT (UDP QUIC) активирован"
      # Цикл от 1 до 8
      for i in {1..8}; do
- 
+
          if [[ $i -ge 2 ]]; then
              prev=$((i - 1))
              echo -n > "/opt/zapret/extra_strats/UDP/YT/${prev}.txt"
          fi
- 
+
          cp "/opt/zapret/extra_strats/UDP/YT/List.txt" "/opt/zapret/extra_strats/UDP/YT/${i}.txt"
- 
+
          /etc/init.d/zapret restart
- 
-         echo "Strategy $i activated"
- 
-         read -p "Test strategy in browser and write answer here (\"Y\" - Apply strategy, Enter for try next strategy): " answer
+
+         echo "Стратегия номер $i активирована"
+
+         read -p "Проверьте работоспособность в браузере и т.п. и дайте ответ: (\"Y\" - Сохранить стратегию, Enter - попробовать следующую): " answer
          clean_answer=$(echo "$answer" | tr -d '[:space:]' | tr '[:lower:]' '[:upper:]')
- 
+
          if [[ "$clean_answer" == "Y" ]]; then
-             echo "Strategy $i applied and exiting."
+             echo "Стратегия $i сохранена. Выходим."
              exit 0
          fi
      done
- 
+
      # После цикла
      echo -n > "/opt/zapret/extra_strats/UDP/YT/8.txt"
-     echo "End strategy lists. All was tried"
+     echo "Перечень стратегий закончился. Мы испробовали всё"
      exit 0
- 	
- elif [[ "$clean_answer" == "2" ]]; then
-     echo "Mode: Finding strategy for YT (TCP) activated"
+	
+  elif [[ "$clean_answer" == "2" ]]; then
+     echo "Режим подбора стратегии для YT (TCP) активирован"
      # Цикл от 1 до 17
      for i in {1..17}; do
- 
+
          if [[ $i -ge 2 ]]; then
              prev=$((i - 1))
              echo -n > "/opt/zapret/extra_strats/TCP/YT/${prev}.txt"
          fi
- 
+
          cp "/opt/zapret/extra_strats/TCP/YT/List.txt" "/opt/zapret/extra_strats/TCP/YT/${i}.txt"
- 
+
          /etc/init.d/zapret restart
- 
-         echo "Strategy $i activated"
- 
-         read -p "Test strategy in browser and write answer here (\"Y\" - Apply strategy, Enter for try next strategy): " answer
+
+         echo "Стратегия номер $i активирована"
+
+         read -p "Проверьте работоспособность в браузере и т.п. и дайте ответ: (\"Y\" - Сохранить стратегию, Enter - попробовать следующую): " answer
          clean_answer=$(echo "$answer" | tr -d '[:space:]' | tr '[:lower:]' '[:upper:]')
- 
+
          if [[ "$clean_answer" == "Y" ]]; then
-             echo "Strategy $i applied and exiting."
+             echo "Стратегия $i сохранена. Выходим."
              exit 0
          fi
      done
- 
+
      # После цикла
      echo -n > "/opt/zapret/extra_strats/TCP/YT/17.txt"
-     echo "End strategy lists. All was tried"
+     echo "Перечень стратегий закончился. Мы испробовали всё"
      exit 0
-     
- elif [[ "$clean_answer" == "3" ]]; then
-     echo "Mode: Finding strategy for RKN lists activated"
+    
+  elif [[ "$clean_answer" == "3" ]]; then
+     echo "Режим подбора стратегии для листа РКН активирован"
      # Цикл от 1 до 17
      for i in {1..17}; do
- 
+
          if [[ $i -ge 2 ]]; then
              prev=$((i - 1))
              echo -n > "/opt/zapret/extra_strats/TCP/RKN/${prev}.txt"
          fi
- 
+
          cp "/opt/zapret/extra_strats/TCP/RKN/List.txt" "/opt/zapret/extra_strats/TCP/RKN/${i}.txt"
- 
+
          /etc/init.d/zapret restart
- 
-         echo "Strategy $i activated"
- 
-         read -p "Test strategy in browser and write answer here (\"Y\" - Apply strategy, Enter for try next strategy): " answer
+
+         echo "Стратегия номер $i активирована"
+
+         read -p "Проверьте работоспособность в браузере и т.п. и дайте ответ: (\"Y\" - Сохранить стратегию, Enter - попробовать следующую): " answer
          clean_answer=$(echo "$answer" | tr -d '[:space:]' | tr '[:lower:]' '[:upper:]')
- 
+
          if [[ "$clean_answer" == "Y" ]]; then
-             echo "Strategy $i applied and exiting."
+             echo "Стратегия $i сохранена. Выходим."
              exit 0
          fi
      done
- 
+
      # После цикла
      echo -n > "/opt/zapret/extra_strats/TCP/RKN/17.txt"
-     echo "End strategy lists. All was tried"
+     echo "Перечень стратегий закончился. Мы испробовали всё"
      exit 0
- 
- elif [[ "$clean_answer" == "4" ]]; then
-     echo "Mode: Finding strategy for new domain activated"
- 
- 	read -p "Input domain please (example, mydomain.com): " user_domain
- 	user_domain=$(echo "$user_domain" | tr -d '[:space:]')  # удаляем лишние пробелы
+
+  elif [[ "$clean_answer" == "4" ]]; then
+     echo "Режим подбора стратегии для кастомного домена активирован"
+
+	 read -p "Введите требуемый домен (Например: mydomain.com): " user_domain
+	 user_domain=$(echo "$user_domain" | tr -d '[:space:]')  # удаляем лишние пробелы
      # Цикл от 1 до 17
      for i in {1..17}; do
- 		#Найти и отключить временно общий лист
- 		for emp in {1..17}; do
- 			file="/opt/zapret/extra_strats/TCP/RKN/${emp}.txt"
- 			if [[ -s "$file" ]]; then
- 				#echo "$emp"
- 				echo -n > "/opt/zapret/extra_strats/TCP/RKN/${emp}.txt"
- 				break
- 			fi
- 		done
- 
+		 #Найти и отключить временно общий лист
+		 for emp in {1..17}; do
+			 file="/opt/zapret/extra_strats/TCP/RKN/${emp}.txt"
+			 if [[ -s "$file" ]]; then
+				 #echo "$emp"
+				 echo -n > "/opt/zapret/extra_strats/TCP/RKN/${emp}.txt"
+				 break
+			 fi
+		 done
+
          if [[ $i -ge 2 ]]; then
              prev=$((i - 1))
              echo -n > "/opt/zapret/extra_strats/TCP/temp/${prev}.txt"
          fi
- 
+
          echo "$user_domain" > "/opt/zapret/extra_strats/TCP/temp/${i}.txt"
- 
+
          /etc/init.d/zapret restart
- 
-         echo "Strategy $i activated"
- 
-         read -p "Test strategy in browser and write answer here (\"Y\" - Apply strategy, Enter for try next strategy): " answer
+
+         echo "Стратегия номер $i активирована"
+
+         read -p "Проверьте работоспособность в браузере и т.п. и дайте ответ: (\"Y\" - Сохранить стратегию, Enter - попробовать следующую): " answer
          clean_answer=$(echo "$answer" | tr -d '[:space:]' | tr '[:lower:]' '[:upper:]')
- 
+
          if [[ "$clean_answer" == "Y" ]]; then
- 			echo -n > "/opt/zapret/extra_strats/TCP/temp/${i}.txt" #Очищаем temp
- 			echo "$user_domain" > "/opt/zapret/extra_strats/TCP/User/${i}.txt" #Кидаем в юзерлист на постоянку в приоритет
- 			cp "/opt/zapret/extra_strats/TCP/RKN/List.txt" "/opt/zapret/extra_strats/TCP/RKN/${emp}.txt" #Включить обратно общий лист
-             echo "Strategy $i applied and exiting."
+			 echo -n > "/opt/zapret/extra_strats/TCP/temp/${i}.txt" #Очищаем temp
+			 echo "$user_domain" > "/opt/zapret/extra_strats/TCP/User/${i}.txt" #Кидаем в юзерлист на постоянку в приоритет
+			 cp "/opt/zapret/extra_strats/TCP/RKN/List.txt" "/opt/zapret/extra_strats/TCP/RKN/${emp}.txt" #Включить обратно общий лист
+             echo "Стратегия $i сохранена. Выходим."
              exit 0
          fi
      done
- 
+
      # После цикла
      echo -n > "/opt/zapret/extra_strats/TCP/temp/17.txt"
-     echo "End strategy lists. All was tried"
+     echo "Перечень стратегий закончился. Мы испробовали всё"
      exit 0
-  
+ 
+  else
+     echo "Пропуск подбора альтернативной стратегии"
+  fi
  else
-     echo "Skipping finding extra strats (default action)"
+  echo "zapret не установлен, пропускаем скрипт подбора профиля"
  fi
  
  #pre
