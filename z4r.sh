aa27051c5947c0093cf2d5a7374094fea6a79614
IndeecFOX <44396407+IndeecFOX@users.noreply.github.com>
2025-08-30 11:46:53 +0300

    Добавление MerlinWRT к OWRT ОС временно возможно

--- z4r.sh
+++ z4r.sh
@@ -393,7 +393,7 @@ echo "OS: $release"
 #Запуск скрипта под нужную версию
 if [[ "$release" == "ubuntu" || "$release" == "debian" || "$release" == "endeavouros" ]]; then
 	OSystem="VPS"
-elif [[ "$release" == "openwrt" || "$release" == "immortalwrt" || "$release" == "asuswrt" ]]; then
+elif [[ "$release" == "openwrt" || "$release" == "immortalwrt" || "$release" == "asuswrt" || "$release" == "merlinWRT" ]]; then
 	OSystem="WRT"
 elif [[ "$release" == "entware" ]]; then
 	OSystem="Entware"
